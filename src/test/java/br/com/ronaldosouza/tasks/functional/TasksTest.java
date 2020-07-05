923146c99cbbaf13f2ac028c0e8440f3c4876d79
Ronaldo Souza <ronaldosouza@MacBook-Pro-de-Ronaldo.local>
2020-07-05 19:38:34 -0300

    Alteracao nas class

--- src/test/java/br/com/ronaldosouza/tasks/functional/TasksTest.java
+++ src/test/java/br/com/ronaldosouza/tasks/functional/TasksTest.java
@@ -1,21 +1,23 @@
 package br.com.ronaldosouza.tasks.functional;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 	
 	public WebDriver acessarAplicacao() throws MalformedURLException {
-		WebDriver driver = new ChromeDriver();
-		//DesiredCapabilities cap = DesiredCapabilities.chrome();
-		//WebDriver driver = new RemoteWebDriver(new URL("http://localhost:8001/tasks"), cap);
-		driver.navigate().to("http://localhost:8001/tasks");
+//		WebDriver driver = new ChromeDriver();
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.100.40:4444/wd/hub"), cap);
+		driver.navigate().to("http://192.168.1.107:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
@@ -39,8 +41,7 @@ public class TasksTest {
 			
 			//validar mensagem de sucesso
 			String message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Sucess!", message); 
-
+			Assert.assertEquals("Success!", message);
 		} finally {			
 			//fechar o browser
 			driver.quit();
@@ -129,7 +130,7 @@ public class TasksTest {
 			driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
 			driver.findElement(By.id("saveButton")).click();
 			String message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Sucess!", message); 
+			Assert.assertEquals("Success!", message);
 			
 			//remover a Tarefa
 			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
