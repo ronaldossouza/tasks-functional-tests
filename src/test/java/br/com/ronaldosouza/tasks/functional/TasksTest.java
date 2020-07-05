20e4abb0f927e78b5226cb2b6e9b4ffec37b4f88
Ronaldo Souza <ronaldosouza@MacBook-Pro-de-Ronaldo.local>
2020-07-05 20:19:00 -0300

    Alterando ip da maquina

--- src/test/java/br/com/ronaldosouza/tasks/functional/TasksTest.java
+++ src/test/java/br/com/ronaldosouza/tasks/functional/TasksTest.java
@@ -17,7 +17,7 @@ public class TasksTest {
 //		WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
 		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.100.40:4444/wd/hub"), cap);
-		driver.navigate().to("http://192.168.1.107:8001/tasks");
+		driver.navigate().to("http://192.168.100.40:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
