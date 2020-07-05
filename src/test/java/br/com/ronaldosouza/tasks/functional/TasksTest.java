13d3ba2986d30c6536dbad7a391711272d1fc913
Ronaldo Souza <ronaldosouza@MacBook-Pro-de-Ronaldo.local>
2020-07-05 20:24:08 -0300

    Alterando ip da maquina

--- src/test/java/br/com/ronaldosouza/tasks/functional/TasksTest.java
+++ src/test/java/br/com/ronaldosouza/tasks/functional/TasksTest.java
@@ -41,7 +41,7 @@ public class TasksTest {
 			
 			//validar mensagem de sucesso
 			String message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Success!", message);
+			Assert.assertEquals("Sucess!", message);
 		} finally {			
 			//fechar o browser
 			driver.quit();
@@ -130,12 +130,12 @@ public class TasksTest {
 			driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
 			driver.findElement(By.id("saveButton")).click();
 			String message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Success!", message);
+			Assert.assertEquals("Sucess!", message);
 			
 			//remover a Tarefa
 			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
 			message = driver.findElement(By.id("message")).getText();
-			Assert.assertEquals("Success!", message);
+			Assert.assertEquals("Sucess!", message);
 		} finally {			
 			//fechar o browser
 			driver.quit();
